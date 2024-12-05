fffd0389c6c9361ee5538539e576750537a36a09
Nabil <nabilfaturr44@gmail.com>
2024-12-05 19:24:19 +0700

    commit

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import multer from "multer";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT_APP || 3000;
 
 const app = express();
 app.use(express.json());
